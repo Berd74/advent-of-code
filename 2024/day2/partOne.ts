3e1edbc1bbf5ec4d9124794bb0a40705774ac42a
bart <bartek.w74@gmail.com>
2024-12-02 07:59:57 +0100

    2024 day 2, part 1 & 2 - Done refactor

--- 2024/day2/partOne.ts
+++ 2024/day2/partOne.ts
@@ -1,21 +1,20 @@
 import {readFile} from '../utils/readFile';
 
 function isReportSafe(report: number[]): boolean {
-  const diff = [];
-
-  report.forEach((r, i, arr) => {
-    if (i === 0) {return}
-    diff.push(r - arr[i-1]);
-  })
-
-  return diff.every((d) => d > 0 && d <= 3) || diff.every((d) => d < 0 && d >= -3);
+  if (report[0] === report[1]) return false;
+  const direction = report[0] < report[1] ? 'up'  : 'down';
+  for (let i = 0; i < report.length; i++) {
+    if (i+1 == report.length) break;
+    const diff = report[i+1] - report[i]
+    if (diff <= 3 && diff > 0 && direction == 'up' || diff >= -3 && diff < 0 && direction == 'down') {continue;}
+    return false
+  }
+  return true
 }
 
 export async function partOne(input: string) {
   const text = await readFile(input);
   const line = text.split('\n');
-  const reports = line
-    .map((l) => l.split(" ").map(Number));
-
-  return reports.filter(r => isReportSafe(r)).length;
+  const reports = line.map((l) => l.split(" ").map(Number));
+  return reports.filter(report => isReportSafe(report)).length
 }
--- 2024/day2/partTwo.ts
+++ 2024/day2/partTwo.ts
@@ -1,34 +1,30 @@
 import {readFile} from '../utils/readFile';
 
 function isReportSafe(report: number[]): boolean {
-  const diff = [];
-
-  report.forEach((r, i, arr) => {
-    if (i === 0) {return}
-    diff.push(r - arr[i-1]);
-  })
-
-  return diff.every((d) => d > 0 && d <= 3) || diff.every((d) => d < 0 && d >= -3);
+  if (report[0] === report[1]) return false;
+  const direction = report[0] < report[1] ? 'up'  : 'down';
+  for (let i = 0; i < report.length; i++) {
+    if (i+1 == report.length) break;
+    const diff = report[i+1] - report[i]
+    if (diff <= 3 && diff > 0 && direction == 'up' || diff >= -3 && diff < 0 && direction == 'down') {continue;}
+    return false
+  }
+  return true
 }
 
-function isReportWithRemovingOne(report: number[]): boolean {
-  if (isReportSafe(report)) return true;
+function isReportSafeExtra(report: number[]): boolean {
+  const safe = isReportSafe(report)
+  if (safe) return true;
 
-  for (let i = 0; i < report.length; i++) {
-    const edited = report.slice(0, i).concat(report.slice(i + 1));
-    if (isReportSafe(edited)) {
-      return true;
-    }
-  }
-
-  return false;
+  return !!report.find((_, i) => {
+    const edited = report.slice(0, i).concat(report.slice(i+1));
+    if (isReportSafe(edited)) return true;
+  })
 }
 
 export async function partTwo(input: string) {
   const text = await readFile(input);
   const line = text.split('\n');
-  const reports = line
-    .map((l) => l.split(" ").map(Number));
-
-  return reports.filter(r => isReportWithRemovingOne(r)).length;
+  const reports = line.map((l) => l.split(" ").map(Number));
+  return reports.filter(report => isReportSafeExtra(report)).length
 }
